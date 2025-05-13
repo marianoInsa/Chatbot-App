dffe4aa7ffe2fb8f9a2a0cf8ed866d29ab4e29ca
marianoInsa <marianoinsaurralde5@gmail.com>
2025-05-13 12:26:38 -0300

    splitting y chunking

---
+++ app/splitting_chunking.py
@@ -0,0 +1,37 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from load_documents import load_documents
+from load_web_page import load_web_page
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def split_documents():
+    """
+    Split documents into smaller chunks for processing.
+    """
+    docs = load_documents("data/documento.pdf")
+    docs += load_web_page([os.getenv('URL_1'),
+            os.getenv('URL_2'),
+            os.getenv('URL_3'),
+            os.getenv('URL_4')])
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50,
+        separators=["\n\n", "\n", " ", ""]
+    )
+
+    chunks = splitter.split_documents(docs)
+
+    return chunks
+
+if __name__ == "__main__":
+    chunks = split_documents()
+    for i, chunk in enumerate(chunks):
+        print(f"Chunk {i + 1}:")
+        print(chunk.page_content)
+        print("==="*20)
+        print(f"Metadata: {chunk.metadata}")
+        print("==="*20)
+        print("\n")
+
