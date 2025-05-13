723537c1ee0422836bd33c07c1d0df4dcaf3e2e5
marianoInsa <marianoinsaurralde5@gmail.com>
2025-05-13 13:49:31 -0300

    embedding y vector db

--- app/splitting_chunking.py
+++ app/splitting_chunking.py
@@ -1,3 +1,4 @@
+from langchain_community.vectorstores.utils import filter_complex_metadata
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from load_documents import load_documents
 from load_web_page import load_web_page
@@ -5,15 +6,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def split_documents():
+def split_documents(docs):
     """
     Split documents into smaller chunks for processing.
     """
-    docs = load_documents("data/documento.pdf")
-    docs += load_web_page([os.getenv('URL_1'),
-            os.getenv('URL_2'),
-            os.getenv('URL_3'),
-            os.getenv('URL_4')])
 
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
@@ -23,10 +19,17 @@ def split_documents():
 
     chunks = splitter.split_documents(docs)
 
-    return chunks
+    cleaned_chunks = filter_complex_metadata(chunks)
+
+    return cleaned_chunks
 
 if __name__ == "__main__":
-    chunks = split_documents()
+    docs = load_documents("data/documento.pdf")
+    docs += load_web_page([os.getenv('URL_1'),
+            os.getenv('URL_2'),
+            os.getenv('URL_3'),
+            os.getenv('URL_4')])
+    chunks = split_documents(docs)
     for i, chunk in enumerate(chunks):
         print(f"Chunk {i + 1}:")
         print(chunk.page_content)
